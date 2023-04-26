618875318fb01ce7fdc19d54b9ac8b2b45c15fb0
liyanakhrl <liyanakhrl@gmaill.com>
2023-04-26 23:03:53 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  isShown = false;
+  ngOnInit(): void {
+
+  }
   title = 'portfolio';
+
+  showMenu(){
+    this.isShown = !this.isShown;
+    const elem = document.querySelector('.menu-list') as HTMLElement;
+    if(this.isShown == true){
+      elem.style.display = 'block';
+    }
+    else{
+      elem.style.display = 'none';
+    }
+  }
+
+ 
+
+
 }
